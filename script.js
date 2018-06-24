cdf2ff004edd84bca2a3a78e94e934d4249029ea
Fadilnackmbl <fadhil.wahyudy24@gmail.com>
2018-06-24 16:35:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script jquery latest 
+https://code.jquery.com/jquery-latest.min.js
+
+// Script font basi asu
+https://fonts.googleapis.com/css?family=Fira+Sans:500,700
+
+// Script ni jg cuma plugin komentar sok sokan js
+https://animebatchwebid.disqus.com/count.js
